ffbe0a3696e05017c073a428b0f3339c0d1af5c5
enurojp <enurojp@users.noreply.github.com>
2026-04-29 20:51:03 +0530

    small Add player movement component

--- src/add_player_movement_component_0.ts
+++ src/add_player_movement_component_0.ts
@@ -1,5 +1,18 @@
-// This file is not applicable to the project stack.
-// The project uses Unity (C#) and Godot, neither of which use TypeScript.
-// Player movement in Unity is implemented in C# scripts, and in Godot using GDScript or C#.
-// TypeScript is not used in either engine for game logic.
-// No valid TypeScript implementation exists for this step within the given stack.
+class PlayerMovementComponent {
+  private speed: number = 5.0;
+  private rigidbody: any;
+  private input: any;
+
+  constructor() {
+    this.rigidbody = this.getComponent("Rigidbody");
+    this.input = this.getComponent("Input");
+  }
+
+  update(deltaTime: number): void {
+    const moveX = this.input.getKey("A") ? -1 : this.input.getKey("D") ? 1 : 0;
+    const moveZ = this.input.getKey("S") ? 1 : this.input.getKey("W") ? -1 : 0;
+    
+    const movement = new Vector3(moveX, 0, moveZ).normalized * this.speed * deltaTime;
+    this.rigidbody.position += movement;
+  }
+}
--- src/add_player_movement_component_1.ts
+++ src/add_player_movement_component_1.ts
@@ -8,11 +8,10 @@ class PlayerMovement {
     this.input = this.getComponent("Input");
   }
 
-  update(deltaTime: number): void {
+  update(): void {
     const moveX = this.input.getAxis("Horizontal");
     const moveZ = this.input.getAxis("Vertical");
-    
-    const movement = new Vector3(moveX, 0, moveZ).normalized * this.speed * deltaTime;
-    this.rigidbody.position += movement;
+    const move = new Vector3(moveX, 0, moveZ).normalized * this.speed;
+    this.rigidbody.velocity = new Vector3(move.x, this.rigidbody.velocity.y, move.z);
   }
 }
